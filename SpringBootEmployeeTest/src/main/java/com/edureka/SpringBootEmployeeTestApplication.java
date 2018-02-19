140e862a972d125615de701a1bf44f5b7fedc725
aprateek <ag.prateek@gmail.com>
2018-02-19 21:57:19 +0530

    Module 5  Solution

---
+++ SpringBootEmployeeTest/src/main/java/com/edureka/SpringBootEmployeeTestApplication.java
@@ -0,0 +1,12 @@
+package com.edureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeTestApplication.class, args);
+	}
+}
